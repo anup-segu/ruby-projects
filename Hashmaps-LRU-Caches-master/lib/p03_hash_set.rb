61964c75bb15698d293433bc7fb5a8f75eaff3b9
anup-segu <anup.segu@gmail.com>
2016-05-17 11:37:27 -0700

    Change DS_Store

---
+++ Hashmaps-LRU-Caches-master/lib/p03_hash_set.rb
@@ -0,0 +1,42 @@
+require_relative 'p02_hashing'
+
+class HashSet
+  attr_reader :count
+
+  def initialize(num_buckets = 8)
+    @store = Array.new(num_buckets) { Array.new }
+    @count = 0
+  end
+
+  def insert(key)
+    self[key.hash] << key
+    @count += 1
+    resize! if @count == num_buckets
+  end
+
+  def include?(key)
+    self[key.hash].include?(key)
+  end
+
+  def remove(key)
+    self[key.hash].delete(key)
+    @count -= 1
+  end
+
+  private
+
+  def [](num)
+    @store[num % num_buckets]
+    # optional but useful; return the bucket corresponding to `num`
+  end
+
+  def num_buckets
+    @store.length
+  end
+
+  def resize!
+    old_store = @store.flatten
+    @store = Array.new(num_buckets * 2) {Array.new}
+    old_store.each {|el| self[el.hash] << el}
+  end
+end
